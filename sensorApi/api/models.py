a79bdbbca89588199525659315e5bfb23d8e38a3
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2020-05-04 00:06:33 +0200

    added models

--- sensorApi/api/models.py
+++ sensorApi/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RegisteredSensors(models.Model):
+    name = models.TextField(max_length=255)
+    description = models.TextField(max_length=1000)
+
+
+class LoggedData(models.Model):
+    temperature = models.FloatField
+    humidity = models.FloatField
+    sensor = models.ForeignKey(RegisteredSensors, on_delete=models.SET_NULL)
